6ee7ed65dc873d59ae8dec464dc48cf343deb8f9
mlstack <30563885+mlstack@users.noreply.github.com>
2018-10-24 01:46:10 +0900

    Update tf-MNIST-mlp-classification.py

--- tf-MNIST-mlp-classification.py
+++ tf-MNIST-mlp-classification.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env /c/Apps/Anaconda3/python
 """
-[Title] CNN - Tensorflow Application - Mnist
-[Code] tf-MNIST-cnn-classificaton.py
+[Title] MLP - Tensorflow Application - Mnist
+[Code] tf-MNIST-mlp-classificaton.py
 [Author] 이이백(Yibeck.Lee@gmail.com)
 """
 import tensorflow as tf 
